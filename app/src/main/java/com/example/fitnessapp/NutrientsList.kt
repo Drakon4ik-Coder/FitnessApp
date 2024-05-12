d606bc781fcb2657696baee2f5cd612ce5c9fd08
Illia <illiadovho@gmail.com>
2024-05-12 16:11:11 +0100

    Added ingredient display

---
+++ app/src/main/java/com/example/fitnessapp/NutrientsList.kt
@@ -0,0 +1,5 @@
+package com.example.fitnessapp
+
+object NutrientsList {
+    var nutrients = mutableMapOf("calories" to "kcal", "fats" to "g", "saturated fats" to "g", "carbs" to "g", "sugars" to "g", "fibers" to "g", "protein" to "g")
+}
