f61aa4dc1fedd2d711161f9af4712fab5f5dd6d1
Illia <illiadovho@gmail.com>
2024-06-20 15:42:34 +0100

    Added MainActivity with bottom navigation bar and empty fragments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,12 +36,16 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.mikhaellopez:circularprogressbar:3.1.0")
     implementation("com.intuit.sdp:sdp-android:1.1.1")
     implementation("androidx.activity:activity-ktx:1.9.0")
+    implementation("androidx.activity:activity:1.9.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     val room_version = "2.6.1"
 
     implementation("androidx.room:room-runtime:$room_version")
---
+++ app/src/main/java/com/example/fitnessapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.fitnessapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+    }
+}
